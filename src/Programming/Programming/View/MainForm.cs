c0c264e8ea2bf781a1349fd307a03ed473ef1a32
Kent 28_ <neya.1964@mail.ru>
2022-03-04 23:26:26 +0700

    Initialize EnumsList

--- src/Programming/Programming/View/MainForm.cs
+++ src/Programming/Programming/View/MainForm.cs
@@ -1,20 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Programming.Model;
 
-namespace Programming
+namespace Programming.View
 {
     public partial class MainForm : Form
     {
         public MainForm()
         {
             InitializeComponent();
+
+            foreach (var enums in Enum.GetValues(typeof(Enums)))
+            {
+                EnumsListBox.Items.Add(enums);
+            }
         }
 
         private void EnumsListBox_SelectedIndexChanged(object sender, EventArgs e)
