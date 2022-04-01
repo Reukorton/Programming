369980457bb117d9dbc81f0e28f70555faabb8fa
Kent 28_ <neya.1964@mail.ru>
2022-04-02 00:41:07 +0700

    Create classe movie

---
+++ src/Programming/Programming/Model/Classes/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model.Classes
+{
+    class Movie
+    {
+        private string _title;
+
+        private int _duration;
+
+        private int _releaseYear;
+
+        private string _genre;
+
+        private double _rating;
+    }
+}
