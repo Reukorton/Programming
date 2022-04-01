28ce490c14c6c518f1915c6e72c6618f5c00972b
Kent 28_ <neya.1964@mail.ru>
2022-04-02 02:39:35 +0700

    Create constructor flight

--- src/Programming/Programming/Model/Classes/Flight.cs
+++ src/Programming/Programming/Model/Classes/Flight.cs
@@ -14,6 +14,13 @@ namespace Programming.Model.Classes
 
         public string Destination { get; set; }
 
+        public Flight(int flightTime, string departure, string destination)
+        {
+            FlightTimeInMinutes = flightTime;
+            Departure = departure;
+            Destination = destination;
+        }
+
         public int FlightTimeInMinutes
         {
             get
