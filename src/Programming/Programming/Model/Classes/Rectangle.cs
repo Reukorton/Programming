7c38f46becc72fdd76a59914139e3ae15e3f6f6c
Kent 28_ <neya.1964@mail.ru>
2022-04-02 01:28:00 +0700

    Accessors method rectangle

--- src/Programming/Programming/Model/Classes/Rectangle.cs
+++ src/Programming/Programming/Model/Classes/Rectangle.cs
@@ -11,5 +11,47 @@ namespace Programming.Model.Classes
         private double _length;
         private double _width;
         private string _color;
+
+        public double Length
+        {
+            get
+            {
+                return _length;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(
+                        "Длина не может быть меньше нуля");
+                }
+
+                _length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(
+                        "Ширина не может быть меньше нуля");
+                }
+
+                _width = value;
+            }
+        }
+
+        public string Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
     }
 }
