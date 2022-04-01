5e678197f49c80e48c276f65234ac3e901c0dec0
Kent 28_ <neya.1964@mail.ru>
2022-04-02 00:51:06 +0700

    Create classe contact

---
+++ src/Programming/Programming/Model/Classes/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model.Classes
+{
+    class Contact
+    {
+        private string _phoneNumber;
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
