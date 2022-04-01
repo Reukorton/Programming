a12a84fa7aafb6a6b370b58cef1b75d8db984040
Kent 28_ <neya.1964@mail.ru>
2022-04-02 00:37:54 +0700

    Create classe flight

---
+++ src/Programming/Programming/Model/Classes/Flight.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model.Classes
+{
+    class Flight
+    {
+        public string Departure { get; set; }
+
+        public string Destination { get; set; }
+
+        public int FlightTimeInMinutes { get; set; }
+    }
+}
