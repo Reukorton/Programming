fa2cc6526197bd656fe1af88a0d3f40e6e96e7b3
Kent 28_ <neya.1964@mail.ru>
2022-04-02 01:04:40 +0700

    Create classes subject

---
+++ src/Programming/Programming/Model/Classes/Subject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model.Classes
+{
+    class Subject
+    {
+        public string Title { get; set; }
+
+        public int Mark { get; set; }
+
+        public string FirstNameTeacher { get; set; }
+
+        public string MiddleNameTeacher { get; set; }
+
+        public string LastNameTeacher { get; set; }
+        
+    }
+}
