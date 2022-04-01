2ab70f682e3dec3c31d2da263b814aaf6ffd3d2f
Kent 28_ <neya.1964@mail.ru>
2022-04-02 00:42:16 +0700

    Create class time

---
+++ src/Programming/Programming/Model/Classes/Time.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model.Classes
+{
+    class Time
+    {
+        private int _hours;
+
+        private int _minutes;
+
+        private int _seconds;
+    }
+}
