67953b02b6256e19671481ac18d7709b5d9eba2b
Kent 28_ <neya.1964@mail.ru>
2022-04-02 02:25:37 +0700

    Accessors method time

--- src/Programming/Programming/Model/Classes/Time.cs
+++ src/Programming/Programming/Model/Classes/Time.cs
@@ -11,5 +11,59 @@ namespace Programming.Model.Classes
         private int _hours;
         private int _minutes;
         private int _seconds;
+        
+        public int Hours
+        {
+            get
+            {
+                return _hours;
+            }
+            set
+            {
+                if (value < 0 || value > 23)
+                {
+                    throw new ArgumentException(
+                        "Неверное значение времени в часах");
+                }
+
+                _hours = value;
+            }
+        }
+
+        public int Minutes
+        {
+            get
+            {
+                return _minutes;
+            }
+            set
+            {
+                if (value < 0 || value > 59)
+                {
+                    throw new ArgumentException(
+                        "Неверное значение времени в минутах");
+                }
+
+                _minutes = value;
+            }
+        }
+
+        public int Seconds
+        {
+            get
+            {
+                return _seconds;
+            }
+            set
+            {
+                if (value < 0 || value > 59)
+                {
+                    throw new ArgumentException(
+                        "Неверное значение времени в секундах");
+                }
+
+                _seconds = value;
+            }
+        }
     }
 }
