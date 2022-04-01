98640b03bb76cff4d577bb95b6eac329f2b8a0d0
Kent 28_ <neya.1964@mail.ru>
2022-04-02 01:47:37 +0700

    Accessors method flight

--- src/Programming/Programming/Model/Classes/Flight.cs
+++ src/Programming/Programming/Model/Classes/Flight.cs
@@ -8,10 +8,28 @@ namespace Programming.Model.Classes
 {
     class Flight
     {
+        private string _flightTimeMinutes;
+
         public string Departure { get; set; }
 
         public string Destination { get; set; }
 
-        public int FlightTimeInMinutes { get; set; }
+        public string FlightTimeInMinutes
+        {
+            get
+            {
+                return _flightTimeMinutes;
+            }
+            set
+            {
+                if (!int.TryParse(value, out int result))
+                {
+                    throw new ArgumentException(
+                        "Неверные символы во времени полета");
+                }
+
+                _flightTimeMinutes = value;
+            }
+        } 
     }
 }
