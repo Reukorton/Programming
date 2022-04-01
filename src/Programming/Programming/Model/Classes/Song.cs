7966c9ede1ac04213841d254215fdba6b436e864
Kent 28_ <neya.1964@mail.ru>
2022-04-02 00:56:02 +0700

    Create classe song

---
+++ src/Programming/Programming/Model/Classes/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model.Classes
+{
+    class Song
+    {
+        private int _seconds;
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Description { get; set; }
+    }
+}
