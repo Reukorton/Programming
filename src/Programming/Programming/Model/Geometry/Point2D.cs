0403ec03e6d48780827b4abfb3b7d9130c1d5425
Kent 28_ <neya.1964@mail.ru>
2022-05-06 01:30:57 +0700

    Point2D

--- src/Programming/Programming/Model/Geometry/Point2D.cs
+++ src/Programming/Programming/Model/Geometry/Point2D.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace Programming.Model.Geometry
 {
-    internal class Point2D
+    public class Point2D
     {
+        private int _x;
+
+        private int _y;
+
+        public Point2D(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                Validator.AssertOnPositiveValue(value, nameof(X));
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                Validator.AssertOnPositiveValue(value, nameof(Y));
+            }
+        }
     }
 }
