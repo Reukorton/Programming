d534f2b569c1759c78681281bf992b61d721efb2
Reukorton <neya.1964@mail.ru>
2022-11-25 02:29:50 +0700

    Class Address

---
+++ src/ObjectOrientedPractics/ObjectOrientedPractics/Model/Address.cs
@@ -0,0 +1,172 @@
+﻿using ObjectOrientedPractics.Services;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    /// <summary>
+    /// Хранит адресс.
+    /// </summary>
+    public class Address
+    {
+        /// <summary>
+        /// Почтовый индекс.
+        /// </summary>
+        private int _index;
+
+        /// <summary>
+        /// Страна/регион.
+        /// </summary>
+        private string _country;
+
+        /// <summary>
+        /// Город (населенный пункт).
+        /// </summary>
+        private string _city;
+
+        /// <summary>
+        /// Улица.
+        /// </summary>
+        private string _street;
+
+        /// <summary>
+        /// Номер дома.
+        /// </summary>
+        private string _building;
+
+        /// <summary>
+        /// Номер квартиры.
+        /// </summary>
+        private string _apartment;
+
+        /// <summary>
+        /// Создает экзепляр класса <see cref="Address"/>
+        /// </summary>
+        public Address() { }
+
+        /// <summary>
+        /// Создает экземпляр класса <see cref="Address"/>
+        /// </summary>
+        /// <param name="index">Почтовый индекс.</param>
+        /// <param name="country">Страна.</param>
+        /// <param name="city">Город.</param>
+        /// <param name="street">Улица.</param>
+        /// <param name="building">Номер дома.</param>
+        /// <param name="apartment">Номер квартиры</param>
+        public Address(int index, string country, string city, string street, string building, string apartment)
+        {
+            Index = index;
+            Country = country;
+            City = city;
+            Street = street;
+            Building = building;
+            Apartment = apartment;
+        }
+
+        /// <summary>
+        /// Возвращает и задает почтовый индекс. <br/>
+        /// Целое шестизначное число.
+        /// </summary>
+        public int Index
+        {
+            get
+            {
+                return _index;
+            }
+            set
+            {
+                ValueValidator.AssertValueInRange(value, 100000, 999999, nameof(Index));
+                _index = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает страну. <br/>
+        /// Строка не более 50 символов.
+        /// </summary>
+        public string Country
+        {
+            get
+            {
+                return _country;
+            }
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 50, nameof(Country));
+                _country = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает город. <br/>
+        /// Строка не более 50 символов.
+        /// </summary>
+        public string City
+        {
+            get
+            {
+                return _city;
+            }
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 50, nameof(City));
+                _city = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает улицу. <br/>
+        /// Строка не более 100 символов.
+        /// </summary>
+        public string Street
+        {
+            get
+            {
+                return _street;
+            }
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 100, nameof(Street));
+                _street = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает номер дома. <br/>
+        /// Строка не более 10 символов.
+        /// </summary>
+        public string Building
+        {
+            get
+            {
+                return _building;
+            }
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 10, nameof(Building));
+                _building = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает номер квартиры. <br/>
+        /// Строка не более 10 символов.
+        /// </summary>
+        public string Apartment
+        {
+            get
+            {
+                return _apartment;
+            }
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 10, nameof(Apartment));
+                _apartment = value;
+            }
+        }
+    }
+}
